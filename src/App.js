caca3b4b8338e54cad685b81749252b5e1ddf0aa
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-11-18 04:01:56 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage';
 import HomePage from './components/Homepage/HomePage';
 import ProtectedRoute from "./utils/ProtectedRoute";
-
+import { checkInstallation } from '../utils/api';
 
 function App() {
+const [isInstalled, setIsInstalled] = React.useState(false);
+const params = new URLSearchParams(window.location.search);
+const shopDomain = params.get("shop");
+
+
+if (params.has("shop") && shopDomain) {
+  console.log(`Shop parameter exists: ${shopDomain}`);
+  const isInstalledVar = checkInstallation(shopDomain);
+  setIsInstalled(isInstalledVar)
+} 
+
+
+
   return (
     <Router>
       <Routes>
@@ -14,7 +27,7 @@ function App() {
         <Route
           path="/dashboard"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute isInstalled={isInstalled} params={params}>
               <HomePage />
             </ProtectedRoute>
           }
--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,34 +1,19 @@
 
 import {useNavigate } from 'react-router-dom';
-import { checkInstallation } from '../utils/api';
-import React from 'react';
 
-const ProtectedRoute = ({ children }) => {
-const [isInstalled, setIsInstalled] = React.useState(false);
-  const shopDomain = new URLSearchParams(window.location.search).get("shop");
+const ProtectedRoute = ({isInstalled, params, children }) => {
 
-  const navigate = useNavigate();
-
-  React.useEffect(() => {
-    const verifyInstallation = async () => {
-      const isInstalledVar = await checkInstallation(shopDomain);
-      if (!isInstalled) {
-        navigate("/error");
-      }
-
-      setIsInstalled(isInstalledVar)
-    };
-
-    if (shopDomain != null) {
-      verifyInstallation();
-
-    }else{
-      navigate("/error");
-    }
+    console.log(isInstalled)
+    console.log(params.has("shop"))
 
 
-  }, [shopDomain, navigate]);
+const navigate = useNavigate();
 
+if(params.has("shop") && !isInstalled){
+
+    navigate("/error")
+
+}
   // Render the child component if installed
   return children;
 };
