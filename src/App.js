3b40b065702d2342cdfe4d33af8776d943fefb63
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-11-17 17:06:51 +0530

    init

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 import React from "react"
 import axios from "axios"
 
-function App() {
-  const [data, setData] = React.useState("")
 
+import { AppProvider } from '@shopify/app-bridge-react';
+function getShopFromUrl() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('shop');  // This retrieves the value of the 'shop' parameter
+}
+
+const shop = getShopFromUrl();
+console.log(shop)
+const config = {
+  apiKey: '1e49496f044a9cc3a5b89a5e12a3b493',
+  shopOrigin: shop, // Pass shop name dynamically
+  forceRedirect: true,
+};
+
+function App() {
+
+  const [data, setData] = React.useState("")
   React.useEffect(() => {
 
     axios.get('https://smart-tailor.onrender.com').then(response => {
@@ -14,23 +29,28 @@ function App() {
     }, []);
   })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {data}
-        </a>
-      </header>
-    </div>
+    <AppProvider config={config}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {data}
+          </a>
+        </header>
+      </div>
+    </AppProvider>
   );
 }
+
+
+
 
 export default App;
